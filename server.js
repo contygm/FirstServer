558a432abbe9ecdc43a4f3c1572e9f0a69288a85
Genevieve Conty <gmireillec@guest-2-00799.dhcp.unc.edu>
2016-10-06 19:24:23 -0400

    good and bad server working

--- server.js
+++ server.js
@@ -1,11 +1,4 @@
 
-// Using the previous example as a guide, create an app that has two web servers.
-// One that listens on port 7000 and one that listens on port 7500.
-	// The one listening on port 7000 will always tell the user something good 
-	// about themselves.
-
-// The one listening on 7500 will always tell the user something bad about themselves.
-
 // ** Bonus **
 // Generate the good / bad phrase randomly from a list of predefined phrases
 // Use the twitter package inside the response to also return a random tweet!
@@ -14,7 +7,8 @@
 var http = require('http');
 
 //Lets define a port we want to listen to
-var PORT = 7000;
+var PORT = 7000; // good one
+var PORT2 = 7500; // bad one
 
 //We need a function which handles requests and send response
 function handleRequest(request, response){
@@ -23,12 +17,18 @@ function handleRequest(request, response){
 
 //Create a server
 var server = http.createServer(handleRequest);
+var server2 = http.createServer(handleRequest);
 
 //Lets start our server
 //takes two parameters: port and callback function 
 
 server.listen(PORT, function(){
     //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+    console.log("You are AWESOME!", PORT);
+});
+
+server2.listen(PORT2, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("You SUCK!", PORT2);
 });
 
